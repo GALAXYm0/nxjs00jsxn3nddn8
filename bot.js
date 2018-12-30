dbf1a22a1e9a74cc37fcc51478e486077757ed17
GALAXYm0 <46260476+GALAXYm0@users.noreply.github.com>
2018-12-30 14:40:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "=";
+var prefix = "=";
 var adminprefix = '='
 /////////////////////////
 ////////////////////////
@@ -358,8 +358,8 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-var prefix = "+";
-var adminprefix = '+'
+var prefix = "=";
+var adminprefix = '='
 const developers = ["307938325799501824"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
