7e4f7136bc9ed7c92304d8a5dfdbc965a80e37d3
GALAXYm0 <46260476+GALAXYm0@users.noreply.github.com>
2019-01-12 15:01:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,7 +355,7 @@ client.on('message', message => {
 if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
-
+});
 	
 	
 	
